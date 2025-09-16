b73418aa63c7c32abbde88c134e29f5f83293bb8
ograndegabriel <146639120+ograndegabriel@users.noreply.github.com>
2025-09-16 12:17:28 -0300

    atua

---
+++ script.js
@@ -0,0 +1,18 @@
+//Criação dos sons com lista
+const sons = {
+    "A" : 'boom.wav',
+    "S" : 'clap.wav',
+    "D" : 'hithat.wav',
+    "j" : 'tom.wav'
+}
+
+const criaDiv = (sons) => {
+    div.classList('key');
+    div.textContent = texto;
+
+}
+
+
+
+exibir(sons);
+document.getElementById('conatainer')
